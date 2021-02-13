dd0dc5e8df86ffd866af5668c4f772b534145f47
alexoreiro <alexis.oreiro@icloud.com>
2021-02-13 16:45:09 +0000

    base class of all our models

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Base Model classs that will be used by all instances """
+
+import models
+from datetime import datetime
+import uuid
+import json
+
+class BaseModel():
+    """BaseModel class that defines all common attributes/methods for other classes"""
+    def __init__(self, *args, **kwargs):
+        """ initialization of an BaseModel instance with attributes """
+        if not (kwargs):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != " class":
+                    setattr(self, key, value)
+
+def __str__(self):
+    """ returns a str representation of a object """
+    return ("[{}] ({}) {}".format(self.__class__.__name__,self.id, self.__dict__))
+
+def save(self)
+    """ updates updated_at with the current datetime """
+    self.updated_at = datetime.now()
+    models.storage.save()
+
+def to_dict(self):
+    """ returns dict containing all key values of __dict__ """
+    instance = self.__dict__.copy()
+    instance['__class__'] = slef.__calss__.__name___
+    instance['created_at'] = self.created_at.isoformat()
+    instance['updated_at'] = self.updated_at.isoformat()
+    return instance
