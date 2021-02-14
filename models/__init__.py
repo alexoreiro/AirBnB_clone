ea2ce173ccdb7549757aa2032ebe8e9486b6d078
alexoreiro <alexis.oreiro@icloud.com>
2021-02-14 22:40:08 +0000

    adding files

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,8 @@
 """ Create a Filestorage instance storage """
 
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
 
 storage = FileStorage()
 storage.reload()
---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""user clas that inherits from basemodel"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+
+""" name: string - empty string """
+name = ""
