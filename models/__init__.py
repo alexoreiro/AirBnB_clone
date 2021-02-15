e1de3ea5339e75476bab1c3f02c612133c74e49f
alexoreiro <alexis.oreiro@icloud.com>
2021-02-15 22:06:58 +0000

    file storage added to init

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
