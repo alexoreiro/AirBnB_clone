8a209b72a6855fe2e1626a652f9d0773f69ac32f
alexoreiro <alexis.oreiro@icloud.com>
2021-02-15 21:49:47 +0000

    file storage file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""FileStorage serializes instances to JSON file and deseralizes fils to instances"""
+
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
