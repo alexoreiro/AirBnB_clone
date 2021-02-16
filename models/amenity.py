406cbb5716561ba7038540334d83f865ad8a1643
alexoreiro <alexis.oreiro@icloud.com>
2021-02-16 23:10:48 +0000

    console v.1.0

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Amenity class that inherits from basemodel
+'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+    name: string -empty string
+    '''
+    name = ""
--- models/state.py
+++ models/state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
-"""user clas that inherits from basemodel"""
-
+"""
+user class that inherits from basemodel
+"""
 from models.base_model import BaseModel
 
-class State(BaseModel):
 
-""" name: string - empty string """
-name = ""
+class State(BaseModel):
+    """
+    name: string - empty string
+    """
+    name = ""
